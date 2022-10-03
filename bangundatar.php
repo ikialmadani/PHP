0bb3d2d8fc744a90924446f48fee5df786d27948
ikialmadani <103030206+ikialmadani@users.noreply.github.com>
2022-10-03 23:51:38 +0700

    Add files via upload

---
+++ bangundatar.php
@@ -0,0 +1,16 @@
+<?php 
+ abstract class BangunDatar 
+ {
+    abstract public function namaBidang();
+    abstract public function luasBidang();
+    abstract public function kelilingBidang();
+
+ }
+ abstract class Bulat 
+ {
+    abstract public function namaBidang();
+    abstract public function luasBidang();
+    abstract public function kelilingBidang();
+
+ }
+ ?>
---
+++ lingkaran.php
@@ -0,0 +1,13 @@
+<?php 
+require_once "bangundatar.php";
+class Lingkaran extends Bulat
+{
+    public $PHI = 3.14 ;
+    public $r = 2;
+
+}
+    public function __construct($PHI, $r){
+        
+    }
+
+?>
